ebf8990e3072c6d754128cc74e6c7782ee7d9caf
Tim Eves <tim_eves@sil.org>
2024-10-16 14:21:22 +0000

    Make the Guides sidebar "dynamic"
    In this starter it's an example of a static sidebar group at the starlight level.  Make it populate from the guides directory the same as the references.

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,10 +13,11 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					autogenerate: { directory: 'guides' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Example Guide', slug: 'guides/example' },
+					// ],
 				},
 				{
 					label: 'Reference',
