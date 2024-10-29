c7006cbac17c84829f77bf0bab506542564669e7
Tim Eves <tim_eves@sil.org>
2024-10-29 09:05:00 +0000

    Set site and base properties correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://silnrsi.github.io',
+	base: 'wstr-sample-site',
 	integrations: [
 		starlight({
 			title: 'Writing Systems Technical Resources',
