9cd2d0be1fb9f59fbc11b77997dfbc6a4f901d58
Sharon Correll <sharon_correll@sil.org>
2025-08-07 13:16:17 -0400

    Added language list to script pages; added short name property.

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ export const collections = {
 
         scrpropcode: z.string().optional(),
         scrpropname: z.string().optional(),
+        scrpropshortname: z.string().optional(),
         scrproptype: z.string().optional(),
         scrpropregion: z.string().optional(),
         scrpropstatus: z.string().optional(),
