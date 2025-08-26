294cab3b34a67fadce880279fedc0da7c0bb1ad4
Tim Eves <tim_eves@sil.org>
2025-08-26 07:17:01 +0000

    Turn on debug mode for gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-                        gtag('config', '${googleAnalyticsId}');`
+                        gtag('config', '${googleAnalyticsId}', { debug_mode: true });`
                 },
                 {
                     tag: 'script',
