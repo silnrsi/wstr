364060ea36a7c6715ba42dfc2369410b6b8207df
Tim Eves <tim_eves@sil.org>
2025-09-12 15:11:16 +0000

    Implementation of shortcode, permlink or shorturl
    This creates an alternate route at the top-level of the site to any page that has a shortcode property in it's frontmatter.

---
+++ src/pages/[shortcode].ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from "astro";
+import { getCollection } from 'astro:content';
+
+export async function getStaticPaths() {
+    const entries = await getCollection('docs', (entry) => ('shortcode' in entry.data));
+    return entries.map((entry) => ({
+        params: { shortcode: entry.data.shortcode },
+        props: { url: '/' + entry.id },
+    }));
+}
+
+export const GET: APIRoute = ({ params, props, request, redirect }) => {
+    return redirect(props.url, 302);
+};
