d771ca6b04b9b7884f32291105349f778984c442
Tim Eves <tim_eves@sil.org>
2025-09-19 19:28:30 +0000

    Remove direct sqlite3 dependency and use only astrodb

--- src/utils/coredata_access.ts
+++ src/utils/coredata_access.ts
@@ -1,11 +1,5 @@
 // Utility functions to access the coredata SQLite database
-import { db as db_, characters as chars, scripts, eq, sql } from 'astro:db';
-import assert from 'node:assert/strict'
-
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-
-console.log(`DB at ${import.meta.env.ASTRO_DB_REMOTE_URL}`);
+import { db, characters as chars, scripts, eq, sql } from 'astro:db';
 
 // @todo Get db path from environment variable set in .env - currently returning undefined
 const COREDATA_DB_PATH: string = process.env.COREDATA_DB_PATH as string || 'src/data/coredata.sqlite';
@@ -20,15 +14,10 @@ async function getCharacterName(usv: string): Promise<string> {
     return ERROR_INVALID_USV;
   }
 
-  const db = await open({
-    filename: COREDATA_DB_PATH,
-    driver: sqlite3.Database
-  });
-
-  const [result_] = await db_.select({ character_name: chars.character_name }).from(chars).where(eq(chars.character_usv, usv));
-  const result = await db.get('SELECT character_name FROM characters WHERE character_usv = ?', usv);
-  assert.deepEqual(result_, result);
-  await db.close();
+  const [result] = await db
+    .select({ character_name: chars.character_name })
+    .from(chars)
+    .where(eq(chars.character_usv, usv));
 
   return result ? result.character_name : ERROR_INVALID_USV;
 }
@@ -37,18 +26,11 @@ export { getCharacterName, ERROR_INVALID_USV };
 
 // Implement getScripts function
 async function getScripts(whereExpression: string = '', orderExpression: string = 'script_code'): Promise<any[]> {
-  const db = await open({
-    filename: COREDATA_DB_PATH,
-    driver: sqlite3.Database
-  });
-
-  const whereClause = whereExpression ? `WHERE ${whereExpression}` : '';
-  const orderClause = orderExpression ? `ORDER BY ${orderExpression}` : '';
-  const query = `SELECT * FROM scripts ${whereClause} ${orderClause}`;
-  const results_ = await db_.select().from(scripts).where(sql.raw(whereExpression)).orderBy(sql.raw(orderExpression))
-  const results = await db.all(query);
-  // assert.deepEqual(results_, results);
-  await db.close();
+  const results = await db
+    .select()
+    .from(scripts)
+    .where(sql.raw(whereExpression))
+    .orderBy(sql.raw(orderExpression))
 
   return results;
 }
@@ -57,15 +39,11 @@ export { getScripts };
 
 // Return a ScriptObject for a given script code
 async function getScriptByCode(code: string): Promise<any> {
-  const db = await open({
-    filename: COREDATA_DB_PATH,
-    driver: sqlite3.Database
-  });
-
-  const [result_] = await db_.select().from(scripts).where(eq(scripts.script_code, code)).limit(1);
-  const result = await db.get('SELECT * FROM scripts WHERE script_code = ? LIMIT 1', code);
-  assert.deepEqual(result_, result);
-  await db.close();
+  const [result] = await db
+    .select()
+    .from(scripts)
+    .where(eq(scripts.script_code, code))
+    .limit(1);
 
   return result;
 }
