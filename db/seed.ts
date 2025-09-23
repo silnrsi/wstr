2d6555efb8378916219bdacd13dce0edc0344f90
Tim Eves <tim_eves@sil.org>
2025-09-23 09:08:29 +0000

    refactor DB seeding to work the same for prod and dev
    Try to make the seed.ts a bit more readable and maintainable.

--- db/seed.ts
+++ db/seed.ts
@@ -1,42 +1,40 @@
+"use strict"
+import { srcDir } from 'astro:config/server';
 import { parse } from 'csv-parse/sync';
-import { readFile, stat } from 'fs/promises';
-import { getTableName, getTableColumns, sql } from 'drizzle-orm';
-import { db, characters, languages, scripts } from 'astro:db';
-
-const data_dir = 'src/data';
-
-async function convert(table: any) {
-	// Load csv file based on table name.
-	const name = getTableName(table);
-	const csv_path = `${data_dir}/${name}.csv`;
-
-	// Read and parse the file in one go.
-	const records = parse(await readFile(csv_path), {
-		bom: true,
-		trim: true,
-		skip_empty_lines: true,
-		columns: true,
-	});
-
-	await regenerate_table(table, records);
-
-	const n_records  = await db.$count(table);
-	console.log(`\t${name}\t${n_records} records loaded.`)
-}
-
-
-function* chunks(array: any[], size: number) {
-	while (array.length > 0) yield array.splice(0, size)
-};
-
-async function regenerate_table(table: any, records: any[]) {
-	const queries: any = chunks(records, 512).map(records => db.insert(table).values(records));
-	await db.delete(table);
-	await db.batch(queries);
-}
+import { readFile, copyFile, stat } from 'fs/promises';
+import { getTableName } from 'drizzle-orm';
+import { db, characters, languages, scripts, } from 'astro:db';
 
 export default async function seed() {
 	await convert(characters);
 	await convert(languages);
 	await convert(scripts);
 }
+
+async function convert(table: any) {
+	// Load csv file based on table name.
+	const name = getTableName(table);
+	const csv_src = new URL(`${srcDir}data/${name}.csv`);
+
+	// Read and parse the file in one go.
+	const records = await readFile(csv_src)
+	.then(content => parse(content, {
+		bom: true,
+		trim: true,
+		skip_empty_lines: true,
+		columns: true,
+	}));
+
+	await regenerate_table(table, records);
+	console.info(`\t${name}: ${await db.$count(table)} records.`)
+}
+
+async function regenerate_table(table: any, records: any[]) {
+	const queries: any = chunks(records, 512).map(c => db.insert(table).values(c));
+	await db.delete(table);
+	await db.batch(queries);
+}
+
+function* chunks(array: any[], size: number) {
+	while (array.length !== 0) yield array.splice(0, size)
+};
