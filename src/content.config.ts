3388a8283117d103e71f630f80d82ab21d65a664
Victor Gaultney <victor_gaultney@sil.org>
2025-10-16 17:53:15 +0100

    Add rudimentary list of docs to Articles Index

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from '@astrojs/starlight/schema';
 
@@ -27,4 +28,15 @@ export const collections = {
       }),
     }),
   }),
+  articlelibdocs: defineCollection({
+    loader: glob({ pattern: ['**/*.md', '**/*.mdx'], base: './src/content/docs/articlelib' }),
+    schema: docsSchema({
+      extend: z.object({
+        subtitle: z.string().optional(),
+        authors: z.string().optional(),
+        shorturl: z.string().optional(),
+      }),
+    }),
+  }),
+  
 };
