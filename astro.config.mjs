39bf7bdaf6d6dfb548885efa65ef787ac4652d59
Sharon Correll <sharon_correll@sil.org>
2025-12-09 15:24:29 -0500

    Added Resources to Reference in side-bar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -109,6 +109,7 @@ export default defineConfig({
                     //autogenerate: { directory: 'reference' },
                     items: [
                         'reference/glossary',
+                        'reference/resources',
                         'reference/standards',
                     ],
                 },
