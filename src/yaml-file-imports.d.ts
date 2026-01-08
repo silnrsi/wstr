13246b7dc18cc7093440c1ac1c8dc99b0932d87b
Tim Eves <tim_eves@sil.org>
2026-01-08 12:56:04 +0000

    Add some structure to the sources.yaml module
    This adds typing information so VSCode and astro check can know what valid sources entry looks like and provide code completion help.

--- src/yaml-file-imports.d.ts
+++ src/yaml-file-imports.d.ts
@@ -1,5 +1,32 @@
 // Specify the file extension you want to import
-declare module "*.yaml" {
-  const value: any; // Add type definitions here if desired
+declare module "*/sources.yaml" {
+  type Entry = {
+      abstract?: string,
+      addendum?: string,
+      author?: string,
+      annotation?: string,
+      booktitle?: string,
+      date?: string,
+      editor?: string,
+      entrytype: NonNullable<"@article" | "@book" | "@inbook" | "@inproceedings" | "@misc" | "@online">,
+      entrysubtype?: 'f',
+      isbn?: string,
+      issn?: string,
+      journaltitle?: string,
+      keywords: [string],
+      location?: string,
+      number?: string,
+      organization?: string,
+      pages?: string,
+      presort?: integer,
+      publisher?: string,
+      series?: string,
+      sortname?: string,
+      title: NonNullable<string>,
+      url?: string,
+      urldate?: string,
+      volume?: string,
+  };
+  const value: Readonly<Record<string, Entry>>; // Add type definitions here if desired
   export default value;
 }
