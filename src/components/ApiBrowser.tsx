a42615e4a5dec379fae1518e37d44d96d27b51a9
Tim Eves <tim_eves@sil.org>
2026-03-16 07:40:24 +0000

    Move inline CSS to React scoped css modules

--- src/components/ApiBrowser.tsx
+++ src/components/ApiBrowser.tsx
@@ -9,9 +9,10 @@ import React, { useState, useEffect } from 'react';
 import { LanguagePicker, languagePickerStrings_en } from 'mui-language-picker'
 import { ThemeProvider, createTheme, type PaletteMode, type Theme } from "@mui/material/styles";
 import Family, {type Props as FamilyProps} from './Family'
+import styles from './ApiBrowser.module.css'
 
 const copyIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-hidden="true" fill="currentColor" style={{width: '1em', height: '1em', colorAdjust: 'economy', verticalAlign: '-0.125em'}}>
-  {/* <!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--> */}
+  // !Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.
   <path d="M288 464L64 464c-8.8 0-16-7.2-16-16l0-224c0-8.8 7.2-16 16-16l48 0 0-48-48 0c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l224 0c35.3 0 64-28.7 64-64l0-48-48 0 0 48c0 8.8-7.2 16-16 16zM224 304c-8.8 0-16-7.2-16-16l0-224c0-8.8 7.2-16 16-16l224 0c8.8 0 16 7.2 16 16l0 224c0 8.8-7.2 16-16 16l-224 0zm-64-16c0 35.3 28.7 64 64 64l224 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L224 0c-35.3 0-64 28.7-64 64l0 224z"/>
 </svg>
 
@@ -87,18 +88,19 @@ function ApiBrowser() {
 
   function presentResponse() {
     if (!data) return <></>
+    console.log(JSON.stringify(styles))
     return (
       <div>
         <details>
           <summary style={{font: 'unset'}}>
             Record for {lgName} ({bcp47}) from LFF version: {data.apiversion}
-            <button style={{float: 'right', background: 'unset', border: 'unset', cursor: 'pointer'}} onClick={copyResponse}>{copyIcon}</button>
+            <button className={styles.copy} onClick={copyResponse}>{copyIcon}</button>
           </summary>
-          <pre style={{overflow: 'auto'}}>
+          <pre className={styles.response}>
             <code>{JSON.stringify(data, null, 2)}</code>
           </pre>
         </details>
-        <ul style={{listStyle: 'none', padding: 'unset', maxHeight: '50vh', paddingRight: '2vw', overflow: 'auto'}}>
+        <ul className={styles.families}>
         {Object.values(data.families as Record<string, FamilyProps>).map((family) => <li><Family {...family} /></li>)}
         </ul>
       </div>
--- src/components/Family.tsx
+++ src/components/Family.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { LanguagePicker, languagePickerStrings_en } from 'mui-language-picker'
 import { ThemeProvider, createTheme, type PaletteMode, type Theme } from "@mui/material/styles";
 import { isAsExpression, type NumericLiteral } from 'typescript';
+import styles from './Family.module.css'
 
 type FontType = "ttf" | "woff" | "woff2"
 
@@ -43,32 +44,36 @@ function describeAxes(axes: Axes | undefined): string | undefined {
 }
 
 const licenseIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" aria-hidden="true" fill="currentColor" style={{display: 'inline', width: '1em', height: '1em', colorAdjust: 'economy', verticalAlign: '-0.125em'}}>
-    {/* !Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc. */}
+    // !Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.
     <path d="M384 32l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L398.4 96c-5.2 25.8-22.9 47.1-46.4 57.3l0 294.7 160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-384 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l160 0 0-294.7c-23.5-10.3-41.2-31.6-46.4-57.3L128 96c-17.7 0-32-14.3-32-32s14.3-32 32-32l128 0c14.6-19.4 37.8-32 64-32s49.4 12.6 64 32zm55.6 288L584.4 320 512 195.8 439.6 320zM512 416c-62.9 0-115.2-34-126-78.9-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1-10.8 44.8-63.1 78.9-126 78.9zM126.8 195.8L54.4 320 199.3 320 126.8 195.8zM.9 337.1c-2.6-11 1-22.3 6.7-32.1l95.2-163.2c5-8.6 14.2-13.8 24.1-13.8s19.1 5.3 24.1 13.8l95.2 163.2c5.7 9.8 9.3 21.1 6.7 32.1-10.8 44.8-63.1 78.9-126 78.9S11.7 382 .9 337.1z"/>
 </svg>
+
+function Lozenge(props: object) {
+    return <span className={styles.lozenge} style={{borderRadius: "1em", paddingInline: "0.4em"}}>
+        {props.children}
+    </span>
+}
 
 export default function Family(props: Props) {
     const { family, defaults, files, license, siteurl, features, sample, version, source } = props
     const types = Object.keys(defaults)
     
     const axes = Array.from(new Set(Object.values(files).map(({ axes }) => describeAxes(axes) ?? 'Undeclared')))
-    const styles = axes.map((style) => <li className='lff-family-style'><span className='lff-family-style-weight'>{style}</span></li>)
-    
-    const lozenge = {borderRadius: "1em", paddingInline: "0.4em", backgroundColor: "AccentColor", marginInline: "0.2em", color: 'AccentColorText',textTransform: "uppercase" }
-    const tech = types.map((type) => <span style={lozenge}>{type}</span>)
+    const weights = axes.map((style) => <li className='lff-family-style'><span className='lff-family-style-weight'>{style}</span></li>)
+    const tech = types.map((type) => <Lozenge>{type}</Lozenge>)
 
-return <details className='lff family' style={{ marginBottom: 'inherit' }}>
-        <summary style={{display: 'flex', fontWeight: 'unset'}}>
-            <span className="lff-family name" style={{flexGrow: 1}}>{siteurl ? <a className="lff-family url" href={siteurl}>{family}</a> : family}</span>
-            <span className='lff-family source' style={{minWidth: '5em',paddingInline: '0.5em'}}>{source}</span>
-            <span className="lff-family license" style={{paddingInline: '0.5em', minWidth: '4em'}}> {licenseIcon} {license}</span>
-            <div style={{display: 'inline', paddingInline: '0.5em', minWidth: '13em'}}>{tech}</div>
+return <details className={styles.family}>
+        <summary>
+            <span className={styles.name}>{siteurl ? <a className="lff-family url" href={siteurl}>{family}</a> : family}</span>
+            <span className={styles.source}>{source}</span>
+            <span className={styles.license}> {licenseIcon} {license}</span>
+            <div className={styles.tech}>{tech}</div>
             <br/>
-            {sample && <img className="lff-family sample" src={sample} alt={`${family} visual sample`}/>}
+            {sample && <img className={styles.sample} src={sample} alt={`${family} visual sample`}/>}
         </summary>
-        {features && <p>Required features: <span>{features}</span></p>}
-        {version && <div>Version: {version}</div>}
-        <h6> Weights </h6>
-        <ul style={{listStyle: 'none', overflow: 'scroll', maxHeight: '6em'}}>{styles}</ul>
+        {features && <p>Required features: <span className={styles.features}>{features}</span></p>}
+        {version && <p>Version: {version}</p>}
+        <h6>Weights</h6>
+        <ul>{weights}</ul>
     </details>
 }
