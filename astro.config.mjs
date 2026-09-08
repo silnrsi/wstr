bed37ecbc75b7ca095f27f1174f251aa409472b5
Tim Eves <tim_eves@sil.org>
2026-09-08 16:07:38 +0000

    Update astro, astrojs/starlight & starlight-links-validator
    This also involves updating the markdown field in the astro.config to use the new unified processor form for specifing plugins.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator';
 import astroBrokenLinksChecker from 'astro-broken-links-checker';
+import { unified } from '@astrojs/markdown-remark'
 import rehypeFigureTitle from 'rehype-figure-title';
 import rehypeExternalLinks from 'rehype-external-links';
 import cookieconsent from "@jop-software/astro-cookieconsent";
@@ -237,33 +238,35 @@ export default defineConfig({
         react(),
     ],
     markdown: {
-        remarkPlugins: [
-            remarkSourcesLinkReference('/biblio/'),
-            remarkCharacterDirectives,
-        ],
-        rehypePlugins: [
-            rehypeFigureTitle, [
-                rehypeExternalLinks, {
-                    target: '_blank', // Open external links in a new tab
-                    rel: ['external', 'nofollow',], // Add security attributes
-                    // Optional: Add content (e.g., an icon) to the end of external links
-                    content: {
-                        type: 'element',
-                        tagName: 'img',
-                        properties: {
-                            src: '/svgs/external-link.svg',
-                            //title: 'External link',
-                            //alt: 'External link',
-                        },
-                        children: [],
-                    },
-                    // Optional: Add attributes to the added content
-                    contentProperties: { 'aria-hidden': true, class: 'external-link-icon' },
-                    // Optional: Filter which <a> tags are processed (e.g., exclude links within code blocks)
-                    selectors: 'a:not(pre a):not(code a)',
-                },
+        processor: unified({
+            remarkPlugins: [
+                remarkSourcesLinkReference('/biblio/'),
+                remarkCharacterDirectives,
             ],
-        ],
+            rehypePlugins: [
+                rehypeFigureTitle, [
+                    rehypeExternalLinks, {
+                        target: '_blank', // Open external links in a new tab
+                        rel: ['external', 'nofollow',], // Add security attributes
+                        // Optional: Add content (e.g., an icon) to the end of external links
+                        content: {
+                            type: 'element',
+                            tagName: 'img',
+                            properties: {
+                                src: '/svgs/external-link.svg',
+                                //title: 'External link',
+                                //alt: 'External link',
+                            },
+                            children: [],
+                        },
+                        // Optional: Add attributes to the added content
+                        contentProperties: { 'aria-hidden': true, class: 'external-link-icon' },
+                        // Optional: Filter which <a> tags are processed (e.g., exclude links within code blocks)
+                        selectors: 'a:not(pre a):not(code a)',
+                    },
+                ],
+            ],
+        })
     },
     redirects: {
         "/guides/app-development-best-practice": "/topics/computing/app-development-best-practice",
